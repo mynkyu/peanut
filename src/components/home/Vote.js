426bfe7de312bffd90ea592260dfef86200cef79
macPro <mynkyu@snu.ac.kr>
2017-09-07 21:12:36 +0900

    Vote css

--- src/components/home/Vote.js
+++ src/components/home/Vote.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
+import  './Vote.css'
+
+import heartImg from '../../main_contest_heart_icon.png'
+
 class Vote extends Component {
     render() {
         return (
-            <div>
-                <div><Link to="/ranking">실시간 투표 상황</Link></div>
+            <div className = "voteSituationLink" >
+                <div className = "heartImage">
+                    <img src = {heartImg} />  
+                </div>
+                
+                <Link to="/ranking" >
+                    <p>실시간 투표 상황</p>
+                </Link>
             </div>
         );
     }
