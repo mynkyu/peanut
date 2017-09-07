a76a5940e2a03c4a82b51a0b343ed9107b2c9fcf
macPro <mynkyu@snu.ac.kr>
2017-09-07 22:07:48 +0900

    Vote css2

--- src/components/home/Vote.js
+++ src/components/home/Vote.js
@@ -8,13 +8,16 @@ import heartImg from '../../main_contest_heart_icon.png'
 class Vote extends Component {
     render() {
         return (
+            
+            
             <div className = "voteSituationLink" >
-                <div className = "heartImage">
-                    <img src = {heartImg} />  
-                </div>
-                
-                <Link to="/ranking" >
+                <Link to="/ranking">
+                    <div className = "heartImage">
+                        <div>
+                            <img src = {heartImg} /> 
+                        </div>
                     <p>실시간 투표 상황</p>
+                    </div>
                 </Link>
             </div>
         );
