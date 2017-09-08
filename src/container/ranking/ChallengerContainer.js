fbfad6c64e7531b9e7f6a7cb4ef5f1317f2a08d3
cheetah132 <mario132@naver.com>
2017-09-08 19:34:10 +0900

    kakao

--- src/container/ranking/ChallengerContainer.js
+++ src/container/ranking/ChallengerContainer.js
@@ -27,7 +27,42 @@ class ChallengerContainer extends Component {
         }, function(error) {
 
         })
-        kakao.shareChallenger(window)
+
+        window.Kakao.Link.createDefaultButton({
+            container: '#kakao-share-challenger',
+            objectType: 'feed',
+            content: {
+              title: '피넛',
+              description: '공유 테스트 입니당',
+              imageUrl: 'http://img.ezmember.co.kr/cache/board/2013/03/18/85c137cf1df080c680d70e457e38f3ba.jpg',
+              link: {
+                mobileWebUrl: 'https://peanut-5b51b.firebaseapp.com/',
+                webUrl: 'https://peanut-5b51b.firebaseapp.com/'
+              }
+            },
+            social: {
+              likeCount: 286,
+              commentCount: 45,
+              sharedCount: 845
+            },
+            buttons: [
+              {
+                title: '웹으로 보기',
+                link: {
+                  mobileWebUrl: 'https://peanut-5b51b.firebaseapp.com/',
+                  webUrl: 'https://peanut-5b51b.firebaseapp.com/'
+                }
+              },
+              {
+                title: '앱으로 보기',
+                link: {
+                  mobileWebUrl: 'https://peanut-5b51b.firebaseapp.com/',
+                  webUrl: 'https://peanut-5b51b.firebaseapp.com/'
+                }
+              }
+            ]
+        });
+        //kakao.shareChallenger(window)
     }
 
     facebookShare() {
