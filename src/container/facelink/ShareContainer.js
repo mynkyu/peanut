98cd14d7c7f41bd6cb4a5f3ea313d776e353de65
cheetah132 <mario132@naver.com>
2017-09-15 22:29:24 +0900

    shareImage

--- src/container/facelink/ShareContainer.js
+++ src/container/facelink/ShareContainer.js
@@ -35,20 +35,20 @@ class ShareContainer extends Component {
         const rightName = '강낭콩'
         const leftNim = '님과'
         const rightNim = '님은'
-        const match = '닮은 얼굴!'
+        const match = '일치!'
         var resultText = ''
         if(similarity < 50) {
             resultText = '괜찮아요... 토닥토닥'
         } else if (similarity < 60){
             resultText = '살짝? 비슷한..것도 같아요.'
         } else if (similarity < 70) {
-            resultText = '느낌이 좀 비슷한데요?'
+            resultText = '느낌이 비슷해요'
         } else if (similarity < 80) {
             resultText = '평소에도 닮았다는 소리 듣죠?'
         } else if (similarity < 90) {
-            resultText = '거의 판박이네요'
+            resultText = '거의 판박이네요.'
         } else {
-            resultText = '본인 사진 넣으셨죠?'
+            resultText = '본인이군요.'
         }
 
         // const leftURL = URL.createObjectURL(face[0].image)
@@ -57,18 +57,40 @@ class ShareContainer extends Component {
 
         const img = new Image();
         img.onload = () => {
-            // const canvas = document.createElement('canvas')
-            const canvas = document.getElementById('tutorial');
+            const canvas = document.createElement('canvas')
+            canvas.width  = 1200
+            canvas.height = 630
+            // const canvas = document.getElementById('tutorial');
             if (canvas.getContext) {
                 var ctx = canvas.getContext('2d');
                 ctx.drawImage(img, 0, 0, 1200, 630);
-                // ctx.fillStyle = '#7d7d7d';
                 
-                ctx.font = 'bold 32px Noto Sans KR';
+                
+                ctx.font = '500 32px Noto Sans KR';
                 ctx.fillStyle = '#f38048';
                 ctx.textAlign = "center"
-                ctx.fillText(leftNim, 381, 450);
-                ctx.fillText(rightNim, 588, 450);
+                ctx.fillText(leftNim, 346, 450);
+                ctx.fillText(rightNim, 583, 450);
+
+                ctx.textAlign = "left"
+                ctx.fillText(match, 420, 510);
+
+                ctx.font = '500 45px Noto Sans KR';
+                ctx.textAlign = "right"
+                ctx.fillText(similarity + '%', 412, 510);
+
+                const leftNameSize = Math.min(38, 180 / leftName.length)
+                ctx.font = '500 ' + leftNameSize +'px Noto Sans KR';
+                ctx.fillStyle = '#7d7d7d';
+                ctx.textAlign = "center"
+                ctx.fillText(leftName, 235, 450, 180);
+
+                const rightNameSize = Math.min(38, 180 / rightName.length)
+                ctx.font = '500 ' + leftNameSize +'px Noto Sans KR';
+                ctx.fillText(rightName, 475, 450, 180);
+
+                ctx.font = '500 32px Noto Sans KR';
+                ctx.fillText(resultText, 391, 560);
 
                 self.drawImage(ctx, leftURL, 268, 278, 102).then((ctx) => {
                     self.drawImage(ctx, rightURL, 515, 278, 102).then((ctx) => {
@@ -133,11 +155,18 @@ class ShareContainer extends Component {
     }
 
     render() {
+        const blob = this.state.shareImageBlob
+        var shareImg = <div></div>
+        if (blob) {
+            const url = URL.createObjectURL(blob)
+            shareImg = <img src={url}/>
+        }
         return (
             <div>
                 <div>
-                    <span className='shareText'>&nbsp;</span>
-                    <canvas id="tutorial" width="1200" height="630"></canvas>
+                    {/* <span className='shareText'>&nbsp;</span> */}
+                    {/* <canvas id="tutorial" width="1200" height="630"></canvas> */}
+                    {shareImg}
                 </div>
                 <div>
                     <button onClick={this.facebookShare} className="shareOnFacebook">
